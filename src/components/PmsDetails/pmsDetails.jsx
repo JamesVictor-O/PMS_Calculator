8a336cb0d754d738ff5b21687bf9f2dcfb1785ac
james victor <victorjames40v@gmail.com>
2024-06-08 00:06:47 +0100

    fix issue: wrote the pms changing function

--- src/components/PmsDetails/pmsDetails.jsx
+++ src/components/PmsDetails/pmsDetails.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react"
 const PmsDetails =()=>{
 
-    const [PmsOpening, setPmsOpening]= useState("")
+    const [Pms, setPms]= useState({
+        openingPMS: "",
+        closingPMS:""
+    });
+
+     const handle_ClosingChange=(event)=>{
+        setPms(prevState => ({
+            ...prevState,
+            openingPMS: event.target.value
+        }))
+     }
     return(
         <div className=" m-4">
             <div className="flex align-middle p-1 justify-center mb-2">
                 <label className="mr-3">Pms Opening :</label>
-                <input type="number" className="outline-none border-black border pl-1"/>
+                <input 
+                  type="number" 
+                  value={Pms.openingPMS} 
+                  className="outline-none border-black border pl-1" 
+                  onChange={handle_ClosingChange}
+                />
             </div>
             <div className="flex align-middle p-1 justify-center mb-2">
                 <label className="mr-3">Pms Closing :</label>
-                <input type="number" className="outline-none border-black border"/>
+                <input value={Pms.closingPMS} type="number" className="outline-none border-black border"/>
             </div>
             <div className="flex align-middle p-1 justify-center mb-2">
                 <label className="mr-3">Liters :</label>
